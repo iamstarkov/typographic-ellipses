9c80a99f4fffa3939017b0a99d578e3fa4f64553
Vladimir Starkov <matmuchrapna@gmail.com>
2015-03-28 04:29:30 +0100

    chore(test): validate test for airbnb config

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
 import ellipsis from './index';
 import { equal, notEqual } from 'assert';
 
-it("should fix single cases", ()=> {
-	equal(ellipsis('foo...bar'), 'foo…bar');
+it('should fix single cases', ()=> {
+  equal(ellipsis('foo...bar'), 'foo…bar');
 });
 
-it("should fix multiple cases", ()=> {
-	equal(ellipsis('foo...bar...moo'), 'foo…bar…moo');
+it('should fix multiple cases', ()=> {
+  equal(ellipsis('foo...bar...moo'), 'foo…bar…moo');
 });
 
-it("should not fix two periods", ()=> {
-	equal(ellipsis('foo..bar'), 'foo..bar');
+it('should not fix two periods', ()=> {
+  equal(ellipsis('foo..bar'), 'foo..bar');
 });
 
-it("should not replace all the things", ()=> {
-	notEqual(ellipsis('foo'), '…');
+it('should not replace all the things', ()=> {
+  notEqual(ellipsis('foo'), '…');
 });
