081aeed2a12cc9743ed7ee2e639cd2baeeb7ff1d
Vladimir Starkov <matmuchrapna@gmail.com>
2015-04-02 23:13:56 +0200

    test(ellipses): add missed multiline test

--- test.js
+++ test.js
@@ -9,6 +9,10 @@ it('should fix multiple cases', ()=> {
   equal(ellipses('foo...bar...moo'), 'foo…bar…moo');
 });
 
+it('should fix multiline strings too', ()=> {
+  equal(ellipses('multiline\n test...'), 'multiline\n test…');
+});
+
 it('should not fix two periods', ()=> {
   equal(ellipses('foo..bar'), 'foo..bar');
 });
